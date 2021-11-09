6b9e898737ba92431c774c8004e35bf28c603215
aktmmiri <lionsfan0316@gmail.com>
2021-11-09 17:05:28 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
-  
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角文字のみ使用可能です'} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options format: {with: /\A[ァ-ヶー－]+\z/, message: 'には全角カナのみ使用可能です'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
 end
