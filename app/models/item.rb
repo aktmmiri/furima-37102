6396f410433ea0ee4210c5ad98718bba9908596f
aktmmiri <lionsfan0316@gmail.com>
2021-11-10 18:09:40 +0900

    出品ページのプルダウン表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordEXtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
