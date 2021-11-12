ec03e5605d6a9d01b7540d2f63667acee36d21db
aktmmiri <lionsfan0316@gmail.com>
2021-11-12 17:06:36 +0900

    カラム名を変えたことによるビューファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :tatus_id, :fee_id, :prefecture_id, :scheduled_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :fee_id, :prefecture_id, :scheduled_id, :price).merge(user_id: current_user.id)
   end
 end
