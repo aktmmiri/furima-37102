0239f181fccd5860899aeec455ed61dc2b33d87f
aktmmiri <lionsfan0316@gmail.com>
2021-11-17 15:00:28 +0900

    エラーメッセージの表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,13 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :order_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number, :token
+    validates :user_id, :item_id, :token
     validates :postal_code, format: { with: /\A[0-9]{3}[-][0-9]{4}\z/, message: '3桁ハイフン4桁で半角数字のみで入力してください' }
-    validates :phone_number, format: { with: /\A[0-9]{10,11}+\z/, message: 'は半角数字のみで入力してください' }
+    validates :city, :house_number
+    validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
+    validates :phone_number, format: { with: /\A[0-9]{10,11}+\z/, message: 'は10または11桁の半角数字のみで入力してください' }
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
+  
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
