18760a6af36f14b4604bc83062b98970aafa46f9
aktmmiri <lionsfan0316@gmail.com>
2021-11-17 14:40:55 +0900

    トークンのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,7 +3,7 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :order_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number
+    validates :user_id, :item_id, :city, :house_number, :token
     validates :postal_code, format: { with: /\A[0-9]{3}[-][0-9]{4}\z/, message: '3桁ハイフン4桁で半角数字のみで入力してください' }
     validates :phone_number, format: { with: /\A[0-9]{10,11}+\z/, message: 'は半角数字のみで入力してください' }
   end
